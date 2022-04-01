66949c0b8e83eef4cc52f30c72c70852ee345fa8
philippe soubrier <philippe.soubrier@gmail.com>
2022-04-01 10:41:15 +0200

    lol

---
+++ test_1.py
@@ -0,0 +1,125 @@
+import PySimpleGUI as sg
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from ortools.linear_solver import pywraplp
+
+# ------------------------------- Google optimization algorithm -------------------------------
+
+
+def create_data_model():
+    """Create the data for the example."""
+    data = {}
+    weights = [48, 30, 19, 36, 36, 27, 42, 42, 36, 24, 30]
+    data['weights'] = weights
+    data['items'] = list(range(len(weights)))
+    data['bins'] = data['items']
+    data['bin_capacity'] = 100
+    return data
+
+
+data = create_data_model()
+
+# Create the mip solver with the SCIP backend.
+solver = pywraplp.Solver.CreateSolver('SCIP')
+
+# Variables
+# x[i, j] = 1 if item i is packed in bin j.
+x = {}
+for i in data['items']:
+    for j in data['bins']:
+        x[(i, j)] = solver.IntVar(0, 1, 'x_%i_%i' % (i, j))
+
+# y[j] = 1 if bin j is used.
+y = {}
+for j in data['bins']:
+    y[j] = solver.IntVar(0, 1, 'y[%i]' % j)
+
+# Constraints
+# Each item must be in exactly one bin.
+for i in data['items']:
+    solver.Add(sum(x[i, j] for j in data['bins']) == 1)
+
+# The amount packed in each bin cannot exceed its capacity.
+for j in data['bins']:
+    solver.Add(
+        sum(x[(i, j)] * data['weights'][i] for i in data['items']) <= y[j] *
+        data['bin_capacity'])
+
+# Objective: minimize the number of bins used.
+solver.Minimize(solver.Sum([y[j] for j in data['bins']]))
+
+status = solver.Solve()
+
+if status == pywraplp.Solver.OPTIMAL:
+    num_bins = 0.
+    for j in data['bins']:
+        if y[j].solution_value() == 1:
+            bin_items = []
+            bin_weight = 0
+            for i in data['items']:
+                if x[i, j].solution_value() > 0:
+                    weight = data['weights'][i]
+                    bin_items.append(weight)
+                    bin_weight += weight
+            if bin_weight > 0:
+                num_bins += 1
+                print('Bin number', j)
+                print('  Items packed:', bin_items)
+                print('  Total weight:', bin_weight)
+                print()
+    print()
+    print('Number of bins used:', num_bins)
+    print('Time = ', solver.WallTime(), ' milliseconds')
+else:
+    print('The problem does not have an optimal solution.')
+
+# ------------------------------- Google optimization algorithm -------------------------------
+
+
+# ------------------------------- PASTE YOUR MATPLOTLIB CODE HERE -------------------------------
+
+fig, ax = plt.subplots()
+
+# Example data
+people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+y_pos = np.arange(len(people))
+performance = 3 + 10 * np.random.rand(len(people))
+error = np.random.rand(len(people))
+
+ax.barh(y_pos, performance, xerr=error, align='center')
+ax.set_yticks(y_pos, labels=people)
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel('Performance')
+ax.set_title('How fast do you want to go today?')
+
+
+# ------------------------------- END OF YOUR MATPLOTLIB CODE -------------------------------
+
+# ------------------------------- Beginning of Matplotlib helper code -----------------------
+
+
+def draw_figure(canvas, figure):
+    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
+    figure_canvas_agg.draw()
+    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+    return figure_canvas_agg
+
+
+# ------------------------------- Beginning of GUI CODE -------------------------------
+
+# define the window layout
+layout = [[sg.Text('Plot test')],
+          [sg.Canvas(key='-CANVAS-')],
+          [sg.Button('Ok')]]
+
+# create the form and show it without the plot
+window = sg.Window('Demo Application - Embedding Matplotlib In PySimpleGUI', layout, finalize=True,
+                   element_justification='center', font='Helvetica 18')
+
+# add the plot to the window
+fig_canvas_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
+
+event, values = window.read()
+
+window.close()
